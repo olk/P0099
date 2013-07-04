bff96aa1f1efbd59d344ab617e720da46e58e8a2
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-07-04 16:28:02 +0200

    update

---
+++ code/asyncres.cpp
@@ -0,0 +1,9 @@
+void echo(boost::asio::ip::tcp::socket & socket,boost::asio::yield_context yield){
+    char data[128];
+    // read asynchronous data from socket
+    // execution context will be suspended until
+    // some bytes could be read from socket
+    std::size_t n=socket.async_read_some(boost::asio::buffer(data),yield);
+    // write some bytes asynchronous to socket
+    boost::asio::async_write(socket,boost::asio::buffer(data,n),yield);
+}
---
+++ code/await.cpp
@@ -0,0 +1,24 @@
+int bar(int i) {
+    // await is not limited by "one level" as in C#
+    auto result = await async([i]{ return reschedule(), i*100; });
+    return result + i*10;
+}
+
+int foo(int i) {
+    cout << i << ":\tbegin" << endl;
+    cout << await async([i]{ return reschedule(), i*10; }) << ":\tbody" << endl;
+    cout << bar(i) << ":\tend" << endl;
+    return i*1000;
+}
+
+void async_user_handler() {
+    vector<future<int>> fs;
+
+    // instead of `async` at function signature, `asynchronous` should be
+    // used at the call place:
+    for(auto i=0; i!=5; ++i)
+        fs.push_back( asynchronous([i]{ return foo(i+1); }) );
+
+    for(auto &&f : fs)
+        cout << await f << ":\tafter end" << endl;
+}
