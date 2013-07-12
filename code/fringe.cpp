f1e163133e0ac04d4bd71f2a4535f47886f415a9
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-07-12 18:40:29 +0200

    remove main() from fringe example

--- code/fringe.cpp
+++ code/fringe.cpp
@@ -14,39 +14,34 @@ node::ptr_t create_tree2(){
         leaf::create("c"));
 }
 
-int main() {
-    // create tree t1
-    node::ptr_t t1=create_tree1();
-    // create a coroutine, recursivly iterates trough t1
-    // returning leaf nodes
-    std::pull_coroutine<leaf&> c1(
-        [&](std::push_coroutine<leaf&> & c){
-            // create visitor, attached to coroutine
-            coro_visitor v(c);
-            // traverse t1 recursivly
-            t1->accept(v);
-        });
+// create tree t1
+node::ptr_t t1=create_tree1();
+// create a coroutine, recursivly iterates trough t1
+// returning leaf nodes
+std::pull_coroutine<leaf&> c1(
+    [&](std::push_coroutine<leaf&> & c){
+        // create visitor, attached to coroutine
+        coro_visitor v(c);
+        // traverse t1 recursivly
+        t1->accept(v);
+    });
 
-    // create tree t2
-    node::ptr_t t2=create_tree2();
-    // create a coroutine, recursivly iterates trough t2
-    // returning leaf nodes
-    std::pull_coroutine<leaf&> c2(
-        [&](std::push_coroutine<leaf&> & c){
-            // create visitor, attached to coroutine
-            coro_visitor v(c);
-            // traverse t2 recursivly
-            t2->accept(v);
-        });
+// create tree t2
+node::ptr_t t2=create_tree2();
+// create a coroutine, recursivly iterates trough t2
+// returning leaf nodes
+std::pull_coroutine<leaf&> c2(
+    [&](std::push_coroutine<leaf&> & c){
+        // create visitor, attached to coroutine
+        coro_visitor v(c);
+        // traverse t2 recursivly
+        t2->accept(v);
+    });
 
-    // compare leaf node ranges of t1 and t2
-    bool result = std::equal(
-            std::begin(c1),
-            std::end(c1),
-            std::begin(c2));
+// compare leaf node ranges of t1 and t2
+bool result = std::equal(
+        std::begin(c1),
+        std::end(c1),
+        std::begin(c2));
 
-    std::cout << std::boolalpha << "same fringe == " << result;
-    std::cout << "\nDone" << std::endl;
-
-    return EXIT_SUCCESS;
-}
+std::cout << std::boolalpha << "same fringe == " << result;
