47bc839ff667ce406f6fbe725557eedc818c6ca9
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-07-15 17:28:54 +0200

    update regarding to Nat's suggestions

--- code/asyncres.cpp
+++ code/asyncres.cpp
@@ -2,8 +2,8 @@ void echo(boost::asio::ip::tcp::socket& socket,boost::asio::yield_context yield)
     char data[128];
     // read asynchronous data from socket
     // execution context will be suspended until
-    // some bytes could be read from socket
+    // some bytes are read from socket
     std::size_t n=socket.async_read_some(boost::asio::buffer(data),yield);
-    // write some bytes asynchronous to socket
+    // write some bytes asynchronously
     boost::asio::async_write(socket,boost::asio::buffer(data,n),yield);
 }
