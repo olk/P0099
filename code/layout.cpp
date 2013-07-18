be2a5b15319dd9fadb535522e5ecac7094f5e5f1
Oliver Kowalke <oliver.kowalke@gmx.de>
2013-07-18 20:41:26 +0200

    Nat's examples added

---
+++ code/layout.cpp
@@ -0,0 +1,38 @@
+struct FinalEOL{
+    ~FinalEOL(){
+        std::cout << std::endl;
+    }
+};
+
+std::vector<std::string> words{
+    "peas", "porridge", "hot", "peas",
+    "porridge", "cold", "peas", "porridge",
+    "in", "the", "pot", "nine",
+    "days", "old" };
+
+int num=5,width=15;
+boost::coroutines::coroutine<std::string>::push_type writer(
+    [&](boost::coroutines::coroutine<std::string>::pull_type& in){
+        // finish the last line when we leave by whatever means
+        FinalEOL eol;
+        // pull values from upstream, lay them out 'num' to a line
+        for (;;){
+            for(int i=0;i<num;++i){
+                // when we exhaust the input, stop
+                if(!in) return;
+                std::cout << std::setw(width) << in.get();
+                // now that we've handled this item, advance to next
+                in();
+            }
+            // after 'num' items, line break
+            std::cout << std::endl;
+        }
+    });
+
+std::copy(std::begin(words),std::end(words),std::begin(writer));
+
+output:
+           peas       porridge            hot           peas       porridge
+           cold           peas       porridge             in            the
+            pot           nine           days            old
+
