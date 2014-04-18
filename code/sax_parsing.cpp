8cb13c2e20796d3bb181de28473b31c2d5d45608
Nat Goodspeed <nat@lindenlab.com>
2014-04-18 12:04:03 -0400

    Update sax_parsing.cpp example. Add descriptive text.

--- code/sax_parsing.cpp
+++ code/sax_parsing.cpp
@@ -3,11 +3,7 @@ struct BaseEvent{
    BaseEvent(const BaseEvent&)=delete;
    BaseEvent& operator=(const BaseEvent&)=delete;
    virtual ~BaseEvent() {}
-   virtual std::ostream& stream(std::ostream&) const=0;
 };
-
-std::ostream& operator<<(std::ostream& out,const BaseEvent& event)
-{ return event.stream(out); }
 
 // End of document or element
 struct CloseEvent: public BaseEvent{
@@ -15,9 +11,6 @@ struct CloseEvent: public BaseEvent{
    CloseEvent(const xml::sax::Parser::TagType& name):
        mName(name)
    {}
-
-   virtual std::ostream& stream(std::ostream& out) const
-   { return out<<"CloseEvent('"<<mName<<"')\n"; }
 
    const xml::sax::Parser::TagType& mName;
 };
@@ -31,15 +24,6 @@ struct OpenEvent: public CloseEvent{
        mAttrs(attrs)
    {}
 
-   virtual std::ostream& stream(std::ostream& out) const{
-       out<<"OpenEvent('"<<mName<<"'";
-       xml::sax::AttributeIterator::OptionalAttribute attribute;
-       while ((attribute=mAttrs.getNext())){
-           out<<", '"<< attribute->name<<"'='"<<attribute->value<<"'";
-       }
-       return out<<")\n";
-   }
-
    xml::sax::AttributeIterator& mAttrs;
 };
 
@@ -49,9 +33,6 @@ struct TextEvent: public BaseEvent{
    TextEvent(xml::sax::CharIterator& text):
        mText(text)
    {}
-
-   virtual std::ostream& stream(std::ostream& out) const
-   { return out<<"TextEvent('"<<mText.getText()<<"')\n"; }
 
    xml::sax::CharIterator& mText;
 };
@@ -90,42 +71,71 @@ void parser(coro_t::push_type& sink,std::istream& in){
                       {
                           sink(TextEvent(text));
                       });
-   // parse the document, firing all the above
-   xparser.parse(in);
+   try
+   {
+       // parse the document, firing all the above
+       xparser.parse(in);
+   }
+   catch (xml::Exception e)
+   {
+       // xml::sax::Parser throws xml::Exception. Helpfully translate the
+       // name and provide it as the what() string.
+       throw std::runtime_error(exception_name(e));
+   }
 }
 
+// Recursively traverse the incoming XML document on the fly, pulling
+// BaseEvent& references from 'events'.
+// 'indent' illustrates the level of recursion.
+// Each time we're called, we've just retrieved an OpenEvent from 'events';
+// accept that as a param.
+// Return the CloseEvent that ends this element.
 const CloseEvent& process(coro_t::pull_type& events,const OpenEvent& context,
                           const std::string& indent=""){
+   // Capture OpenEvent's tag name: as soon as we advance the parser, the
+   // TagType& reference bound in this OpenEvent will be invalidated.
    xml::sax::Parser::TagType tagName = context.mName;
-   // Each time we're called, we've just retrieved 'context' from 'events' --
-   // so the first thing we have to do is pass control to 'events' to let it
-   // deliver the next event. Of course, each time we come back we must
-   // check for the end of the results stream.
+   // Since the OpenEvent is still the current value from 'events', pass
+   // control back to 'events' until the next event. Of course, each time we
+   // come back we must check for the end of the results stream.
    while(events(),events){
+       // Another event is pending; retrieve it.
        const BaseEvent& event=events.get();
-       // std::cout << event;
        const OpenEvent* oe;
        const CloseEvent* ce;
        const TextEvent* te;
        if((oe=dynamic_cast<const OpenEvent*>(&event))){
+           // When we see OpenEvent, recursively process it.
            process(events,*oe,indent+"    ");
        }
        else if((ce=dynamic_cast<const CloseEvent*>(&event))){
+           // When we see CloseEvent, validate its tag name and then return
+           // it. (This assert is really a check on xml::sax::Parser, since
+           // it already validates matching open/close tags.)
            assert(ce->mName == tagName);
            return *ce;
        }
        else if((te=dynamic_cast<const TextEvent*>(&event))){
-           std::cout<<indent<<"'"<<te->mText.getText()<<"'\n";
+           // When we see TextEvent, just report its text, along with
+           // indentation indicating recursion level.
+           std::cout<<indent<<"text: '"<<te->mText.getText()<<"'\n";
        }
    }
 }
 
-// stream XML-document
+// pretend we have an XML file of arbitrary size
 std::istringstream in(doc);
-coro_t::pull_type events(boost::bind(parser,_1,boost::ref(in)));
-// We fully expect at least ONE event.
-assert(events);
-// This dynamic_cast<&> is itself an assertion that the first event is an
-// OpenEvent.
-const OpenEvent& context=dynamic_cast<const OpenEvent&>(events.get());
-process(events, context);
+try
+{
+   coro_t::pull_type events(boost::bind(parser,_1,boost::ref(in)));
+   // We fully expect at least ONE event.
+   assert(events);
+   // This dynamic_cast<&> is itself an assertion that the first event is an
+   // OpenEvent.
+   const OpenEvent& context=dynamic_cast<const OpenEvent&>(events.get());
+   process(events, context);
+}
+catch (std::exception& e)
+{
+   std::cout << "Parsing error: " << e.what() << '\n';
+}
