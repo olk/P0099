ae7218fc6ce4b914d0d38a1c30028ff6302f30df
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-04-02 17:10:50 +0200

    add cycle graph

---
+++ code/N4398/cycle.cpp
@@ -0,0 +1,16 @@
+// N4398: stackless execution context
+int main(){
+    std::execution_context* other=nullptr;
+    auto ctx1=[other]() resumable {
+        (* other)(); // suspend ctx1, resume other
+    };
+    auto ctx2=[&ctx1]() resumable {
+        ctx1(); // suspend ctx2; resume ctx1
+    };
+    auto ctx3=[&ctx1,&ctx2]() resumable {
+        ctx2(); // suspend ctx3, resume ctx2
+        ctx1(); // suspend ctx3, resume ctx1
+    };
+    other=&ctx3;
+    ctx3(); // suspend main context, resume ctx3
+}
