4932bd8cbe0a6407816b0e779746e8aa16693eac
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-04-10 07:01:26 +0200

    enhance hint description + example

--- code/N4397/rl1.cpp
+++ code/N4397/rl1.cpp
@@ -1,7 +1,8 @@
 // N4397: stackful execution context
-std::execution_context l1=[]()resumable(fixedsize(1024)){
+std::execution_context l1=std::execution_context::current();
+auto l2=[&l1]()resumable(fixedsize(1024)){
     ...
 };
-auto l2=[&l1]()resumable(segmented(1024)){
+auto l3=[&l2]()resumable(segmented(1024)){
     ...
 };
