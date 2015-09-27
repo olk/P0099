7d5a636081248442ad71e10e1eec76217b40284c
Oliver Kowalke <oliver.kowalke@gmx.de>
2015-09-27 16:03:33 +0200

    asio example using coroutines

---
+++ code/P0099/coro.cpp
@@ -0,0 +1,14 @@
+void ready_msg(yield_context yield) {
+    try {
+        array< char, 64 > bf1;
+        async_read(socket, buffer(b1), yield);
+        header hdr(bf1);
+        std::size_t n = hdr.payload_size();
+        std::vector< char > b2(n, '\0');
+        async_read(socket, buffer(bf2), yield);
+        payload pld(bf2);
+        // process message ...
+    } catch (exception const&) {
+        // ...
+    }
+}
