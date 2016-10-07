dfca502a4d510f428b152377b302e407f537294c
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-10-07 19:57:54 +0200

    pass execution_context as universal reference to context-fn

--- code/ontop.cpp
+++ code/ontop.cpp
@@ -1,13 +1,13 @@
-std::execution_context<int> f1(std::execution_context<int> ctx,int data) {
+std::execution_context<int> f1(std::execution_context<int> && ctx,int data) {
     std::cout << "f1: entered first time: " << data << std::endl;
     std::tie(ctx,data)=ctx(data+1);
     std::cout << "f1: entered second time: " << data << std::endl;
     std::tie(ctx,data)=ctx(data+1);
     std::cout << "f1: entered third time: " << data << std::endl;
-    return ctx;
+    return std::move(ctx);
 }
 
-std::tuple<std::execution_context<int>,int> f2(std::execution_context<int> ctx,int data) {
+std::tuple<std::execution_context<int>,int> f2(std::execution_context<int> && ctx,int data) {
     std::cout << "f2: entered: " << data << std::endl;
     return std::make_tuple(std::move(ctx),-1);
 }
