2f7c600348660ae536ff93e2daa5883340276a77
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-10-15 09:30:28 +0200

    code formating and noexcept removed from deleted functions

--- code/ec.cpp
+++ code/ec.cpp
@@ -1,19 +1,17 @@
-template<typename ... Args>
+template<typename ...Args>
 class execution_context {
 public:
     execution_context() noexcept;
 
-    template<typename Fn,
-             typename ... Params
-    >
-    execution_context(Fn&& fn, Params&& ... params);
-
     template<typename StackAlloc,
              typename Fn,
-             typename ... Params
-    >
+             typename ...Params>
     execution_context(std::allocator_arg_t, StackAlloc salloc,
-                      Fn&& fn, Params&& ... params);
+                      Fn&& fn, Params&& ...params);
+
+    template<typename Fn,
+             typename ...Params>
+    execution_context(Fn&& fn, Params&& ...params);
 
     ~execution_context();
 
@@ -28,11 +26,11 @@ public:
     operator=(const execution_context& other) noexcept = delete;
 
     std::tuple<execution_context, Args ...>
-    operator()(Args ... args);
+    operator()(Args ...args);
 
     template<typename Fn>
     std::tuple<execution_context, Args ...>
-    operator()(std::invoke_ontop_arg_t, Fn&& fn, Args ... args);
+    operator()(std::invoke_ontop_arg_t, Fn&& fn, Args ...args);
 
     explicit operator bool() const noexcept;
 
