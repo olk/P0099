afdea72451046766d318680d38c876b6f19cdb99
Oliver Kowalke <oliver.kowalke@gmx.de>
2016-10-15 11:03:03 +0200

    changed wording for top-level functions represented by execution_context

--- code/simple.cpp
+++ code/simple.cpp
@@ -1,14 +1,8 @@
 int main() {
-    // P0099: stackful execution context
-    std::execution_context<void> l2(
-        // l1 represents suspended context
-        [](std::execution_context<void>&& l1){
-            std::cout << "inside l2" << std::endl;
-            // resume context l1
-            return std::move(l1);
+    std::execution_context<void> ctx1(
+        [](std::execution_context<void>&& ctx2){ // (b)
+            return std::move(ctx2); // (c)
         });
-    // suspend current context (->l1)
-    // resume context of l2
-    l2();
+    ctx1(); // (a)
     return 0;
 }
